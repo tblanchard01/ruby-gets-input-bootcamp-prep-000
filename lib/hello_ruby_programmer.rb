d9ac060a3301db59516639467055137c7fe0a4dc
tblanchard01 <tim.blanchard@hotmail.co.uk>
2018-03-16 21:06:47 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,5 +1,16 @@
 
+<<<<<<< HEAD
 def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end 
 
+=======
+puts "Please enter your name so that we can greet you more personally:" 
+name =gets.strip
+
+def greeting(name)
+puts "Hi, #{name} Welcome to the wonderful world of Ruby programming."
+end 
+
+greeting(name)
+>>>>>>> 5b01b1de452b74224ba46283f0f7cbd48c4c369b
